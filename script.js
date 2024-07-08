ba4864d5eee9b28bd3bf0e7c86f208790b34a875
jchentutor <jchentutor@gmail.com>
2024-07-08 10:16:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+    let prevButton = document.querySelector(".prev");
+    let nextButton = document.querySelector(".next");
+
+    function showSlide(index) {
+        slides[currentSlide].classList.remove("active");
+        currentSlide = (index + slides.length) % slides.length;
+        slides[currentSlide].classList.add("active");
+    }
+
+    function nextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function prevSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    nextButton.addEventListener("click", nextSlide);
+    prevButton.addEventListener("click", prevSlide);
+
+    // Initial display
+    slides[currentSlide].classList.add("active");
+});
