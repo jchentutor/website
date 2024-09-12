1a0faddbcd10f00d6880ba8fc777d1bb8d672b82
jchentutor <jchentutor@gmail.com>
2024-09-11 18:46:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ window.addEventListener('scroll', function() {
     booktrial.classList.remove('scroll');
   }
 });
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.error(`Section with id "${sectionId}" not found.`);
+    }
+}
